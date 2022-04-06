177e6856fc53f830d9c5ab452cda427bb1e17acc
hugo miranda <hugo.miranda.souza@gmail.com>
2022-04-06 10:57:54 -0300

    add code

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+type ObjectOfType<T, V> = { [P in keyof T as T[P] extends V ? P : never]: P };
+
+type ExcludeKeysOfType<T, V> = {
+  [P in Exclude<keyof T, keyof ObjectOfType<T, V>>]: T[P];
+};
+
+type PartialObjectOrArrayKeysOf<T extends [] | {}> = {
+  [key in keyof T]?: T[key] extends unknown[]
+    ?
+        | Array<keyof ExcludeKeysOfType<T[key][number], object>>
+        | SelectionSet<T[key][number]>
+    : SelectionSet<T[key]>;
+};
+
+export type SelectionSet<T> = T extends {}
+  ? Array<
+      | keyof ExcludeKeysOfType<T, object>
+      | PartialObjectOrArrayKeysOf<
+          ExcludeKeysOfType<T, string | number | boolean | Date>
+        >
+    >
+  : Array<PartialObjectOrArrayKeysOf<T>>;
+
+export function buildSelectionSet<T>(query: SelectionSet<T>): string {
+  const serializedQuery = JSON.stringify(query);
+
+  return serializedQuery
+    .replace(/,|:/g, " ")
+    .replace(/"|{|}/g, "")
+    .replace(/\[/g, "{")
+    .replace(/\]/g, "}");
+}
